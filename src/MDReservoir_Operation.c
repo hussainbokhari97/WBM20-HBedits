16bf0104f59bb7ad281ae5156a29172711528269
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2021-08-20 12:37:30 -0400

    MIsery continues.
    Signed-off-by: Balazs M. Fekete <bfekete@ccny.cuny.edu>

--- src/MDReservoir_Operation.c
+++ src/MDReservoir_Operation.c
@@ -75,7 +75,7 @@ static void _MDReservoirWisser (int itemID) {
 			resStorage = 0.0;
 		}
 		resStorageChg  = resStorage - prevResStorage;	
-		resExtRelease  = resRelease > discharge ? resRelease + (resExtRelease < discharge ? resExtRelease : discharge) - discharge: 0.0;
+		resExtRelease  = resRelease > discharge ? resRelease - discharge: 0.0;
 	}
 	MFVarSetFloat (_MDOutResStorageID,            itemID, resStorage);
 	MFVarSetFloat (_MDOutResStorageChgID,         itemID, resStorageChg);
@@ -117,7 +117,7 @@ static void _MDReservoirOptimized (int itemID) {
 			resRelease = discharge;
 
 		resStorage    = prevResStorage + (discharge - resRelease) * dt / 1e9;
-		resExtRelease = resRelease > discharge ? resRelease + (resExtRelease < discharge ? resExtRelease : discharge) - discharge : 0.0;
+		resExtRelease = resRelease > discharge ? resRelease - discharge : 0.0;
 	}
 	MFVarSetFloat (_MDOutResStorageID,            itemID, resStorage); 
 	MFVarSetFloat (_MDOutResStorageChgID,         itemID, resStorageChg); 
