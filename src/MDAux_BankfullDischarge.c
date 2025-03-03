151367c32d314a9434ab0717798a7cfa0104744f
hussainbokhari97 <33993224+hussainbokhari97@users.noreply.github.com>
2025-03-03 14:49:41 -0500

    Update MDAux_BankfullDischarge.c

--- src/MDAux_BankfullDischarge.c
+++ src/MDAux_BankfullDischarge.c
@@ -6,7 +6,19 @@ static int _MDOutAux_BankfullDischargeID = MFUnset;
 
 static void _MDAux_BankfullDischarge(int itemID) {
     float meanDischarge = MFVarGetFloat(_MDInAux_MeanDischargeID, itemID, 0.0);
-    float bankfullDischarge = 10 * meanDischarge;
+    float factor;
+
+    if (meanDischarge >= 1000) {
+        factor = 5;
+    } else if (meanDischarge >= 100) {
+        factor = 8;
+    } else if (meanDischarge >= 10) {
+        factor = 9;
+    } else {
+        factor = 25;
+    }
+
+    float bankfullDischarge = factor * meanDischarge;
     MFVarSetFloat(_MDOutAux_BankfullDischargeID, itemID, bankfullDischarge);
 }
 
